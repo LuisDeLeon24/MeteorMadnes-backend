b9f26318105a1573bdd6b3177845284138df749e
LuisDeLeon24 <168132018+LuisDeLeon24@users.noreply.github.com>
2025-08-31 17:48:50 -0600

    pruebas, arreglos e implementación de APIs

--- src/fetch/FetchRoutes.js
+++ src/fetch/FetchRoutes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { getHorizons, getESA } from "./FetchController.js";
+import { Horizons, getESA } from "./FetchController.js";
 
 const router = Router();
 
-router.get("/horizons", getHorizons);
-router.get("/ESA", getESA);
+
+
+router.get(
+    "/horizons/:id",
+    Horizons
+    );
+
+router.get(
+    "/ESA",
+     getESA
+    );
 
 export default router;
